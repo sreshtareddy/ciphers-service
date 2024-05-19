0a07472ffd8f4cfe3394a6b02cd4b6141a917b70
sreshtareddy <sreshtareddy001@gmail.com>
2024-05-19 19:08:40 -0400

    Practicum7

---
+++ ciphers_project/api/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+from .ciphers import caesar_encode
+
+# Create your views here.
+def greetings(request):
+    result = {"message":"Welcome to ciphers service!"}
+    return JsonResponse(result)
+
+
+def encode(request, plaintext, shift):
+    parameters = dict(request.GET)
+    print(parameters)
+    cipher = caesar_encode(plaintext,shift)
+    return JsonResponse({"cipher": cipher})
